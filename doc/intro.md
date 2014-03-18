1e7eaffacee07ee1dcdf5ffda9296189a3d97a71
Yi Wang <e@yi-wang.me>
2014-03-18 15:29:07 -0400

    init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to marceline-lab
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject marceline-lab "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [yieldbot/marceline "0.1.0"]
+                 ])
