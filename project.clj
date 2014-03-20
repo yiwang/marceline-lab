76f4f8e800d1acd3890809a6485c00cef4846b61
Yi Wang <e@yi-wang.me>
2014-03-20 16:14:18 -0400

    world count

--- project.clj
+++ project.clj
@@ -3,6 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :aot :all
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [yieldbot/marceline "0.1.0"]
-                 ])
+                 [storm "0.9.0.1"]
+                 ]
+  :min-lein-version "2.0.0"
+  )
--- src/marceline_lab/core.clj
+++ src/marceline_lab/core.clj
@@ -1,6 +1,45 @@
-(ns marceline-lab.core)
+(ns marceline-lab.core
+  (:require [marceline.storm.trident :as t])
+  (:import storm.trident.TridentTopology
+            [storm.trident.testing FixedBatchSpout MemoryMapState$Factory]
+            [backtype.storm LocalDRPC LocalCluster StormSubmitter])
+  (:use [backtype.storm config])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn mk-fixed-batch-spout [max-batch-size]
+  (FixedBatchSpout.
+    ;; Name the tuples that the spout will emit.
+    (t/fields "sentence")
+    max-batch-size
+    (into-array (map t/values '("lord ogdoad"
+                                     "master of level eight shadow world"
+                                     "the willing vessel offers forth its pure essence")))))
+
+(defn mk-topology []
+  (let [trident-topology (TridentTopology.)
+        spout (doto (mk-fixed-batch-spout 3)
+                (.setCycle true))]
+    (t/new-stream trident-topology "word-counts" spout)
+    trident-topology))
+
+
+(defn run-local! []
+  (let [cluster (LocalCluster.)]
+    (.submitTopology cluster "wordcounter" {TOPOLOGY-DEBUG true} (.build (mk-topology)))
+    (Thread/sleep 10000)
+    (.shutdown cluster)
+    ))
+
+(defn submit-topology! [name]
+  (StormSubmitter/submitTopology
+    name
+    {TOPOLOGY-DEBUG true
+     TOPOLOGY-WORKERS 3}
+    (.build (mk-topology))))
+
+(defn -main
+  ([]
+    (run-local!))
+  ([name]
+    (submit-topology! name)))
+
