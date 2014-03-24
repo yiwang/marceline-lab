1960e09ec52a085d784696534b244128690599c7
Yi Wang <e@yi-wang.me>
2014-03-24 17:04:24 -0400

    lein pom

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :aot :all
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :profiles {:provided {:dependencies [[storm "0.9.0.1"]]}}
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [yieldbot/marceline "0.1.0"]
-                 [storm "0.9.0.1"]
                  ]
   :min-lein-version "2.0.0"
   )
--- test/marceline_lab/core_test.clj
+++ test/marceline_lab/core_test.clj
@@ -3,5 +3,5 @@
             [marceline-lab.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "1=1."
+    (is (= 1 1))))
